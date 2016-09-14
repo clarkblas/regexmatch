17fc6621b9439cfdd15e6ab28f6f1902b2e93357
cdblas <cdblas@192.168.56.1>
2016-09-15 07:52:22 +0800

    Initial commit

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,8 +1,7 @@
 package javaapplication1;
-
 public class JavaApplication1 {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
        
     }
     
