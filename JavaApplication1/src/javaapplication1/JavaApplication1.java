13178dfa270eb860202f80ec885a7a9c1050808b
cdblas <cdblas@192.168.56.1>
2016-09-15 08:21:15 +0800

    Adds first regex

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,12 +1,18 @@
 package javaapplication1;
 public class JavaApplication1 {
    
+    
     public static boolean isTrue(String str)
        {
        return str.matches("[tT]true");
        }
+    public static boolean containsFalse(String str)
+       {
+       return str.matches(".*false.*");
+       }
     public static void main(String[] args)
     {
         System.out.println(isTrue("True"));
+        System.out.println(containsFalse("adhfahasdfalsehasdha"));
     }
 }
