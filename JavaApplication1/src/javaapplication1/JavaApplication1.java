241d61458481325ec993a4a7bcf51f6f898e2ee9
cdblas <cdblas@192.168.56.1>
2016-09-15 08:14:10 +0800

    Adds first regex

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,8 +1,12 @@
 package javaapplication1;
 public class JavaApplication1 {
-    public static void main(String[] args) 
+   
+    public static boolean isTrue(String str)
+       {
+       return str.matches("[t/T]true");
+       }
+    public static void main(String[] args)
     {
-       
+        System.out.println(isTrue("True"));
     }
-    
 }
